c1636faa28da5e258fd01a5c4abfd458b5619dd9
B5WANG <neo.b5wang@gmail.com>
2023-02-05 19:58:44 +0800

    log maker
    log maker

--- ELK/elk-demo/build.sh
+++ ELK/elk-demo/build.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
 docker-compose down -v
-rm -rf ./zookeeper-0/data
-rm -rf ./kafka-0/data
-mkdir -p ./zookeeper-0/data
-mkdir -p ./kafka-0/data
+
+rm -rf ./logstash/data
+rm -rf ./logstash/logs
+rm -rf ./elasticsearch/data
+rm -rf ./elasticsearch/plugins
+
+mkdir -p ./logstash/data
+mkdir -p ./logstash/logs
+mkdir -p ./elasticsearch/data
+mkdir -p ./elasticsearch/plugins
+
 docker-compose build
 docker-compose up -d
---
+++ ELK/logmaker/src/main/java/com/b5wang/elk/logmaker/LogMakerStarter.java
@@ -0,0 +1,13 @@
+package com.b5wang.elk.logmaker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogMakerStarter {
+
+    public static void main(String[] args){
+        SpringApplication.run(LogMakerStarter.class,args);
+    }
+
+}
