2e87118aec4ea4b76b5c010aa4373f85fb292d0b
B5WANG <neo.b5wang@gmail.com>
2023-03-06 00:11:51 +0800

    redis single
    redis single

--- redis/redis-single/build.cmd
+++ redis/redis-single/build.cmd
@@ -1,20 +1,6 @@
 #windows cmd
-
 docker-compose down -v
-
-rmdir zoo1\data
-rmdir zoo2\data
-rmdir zoo3\data
-rmdir zoo1\datalog
-rmdir zoo2\datalog
-rmdir zoo3\datalog
-
-mkdir zoo1\data
-mkdir zoo2\data
-mkdir zoo3\data
-mkdir zoo1\datalog
-mkdir zoo2\datalog
-mkdir zoo3\datalog
-
+rmdir data
+mkdir data
 docker-compose build
 docker-compose up -d
