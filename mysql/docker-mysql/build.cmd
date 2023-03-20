d235f17b8dbbea5db78cd483a197394ec4e507c2
B5WANG <neo.b5wang@gmail.com>
2023-03-20 22:42:08 +0800

    Fix mysql single instance in win11
    Fix mysql single instance in win11

--- mysql/docker-mysql/build.cmd
+++ mysql/docker-mysql/build.cmd
@@ -1,6 +1,7 @@
 # windows cmd
+
 docker-compose down -v
-rmdir mysql-single\data
+rmdir mysql-single\data /s /q
 mkdir mysql-single\data
-docker-compose build
+docker-compose build --no-cache
 docker-compose up -d
