241f28b2e91681a93810da2f5ef59b803b391e3d
B5WANG <neo.b5wang@gmail.com>
2024-01-11 08:00:01 +0800

    spring rabbitmq
    spring rabbitmq

---
+++ messaging/rabbitmq/java/rabbitmq-producer/src/main/java/com/b5wang/architect/rabbitmqproducer/RabbitmqProducerApplication.java
@@ -0,0 +1,13 @@
+package com.b5wang.architect.rabbitmqproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RabbitmqProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RabbitmqProducerApplication.class, args);
+	}
+
+}
