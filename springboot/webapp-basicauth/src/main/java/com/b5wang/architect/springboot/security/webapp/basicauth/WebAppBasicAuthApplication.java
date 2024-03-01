37d0ec288ed7da124b24c03baec82b5911033d18
B5WANG <neo.b5wang@gmail.com>
2024-03-01 18:06:49 +0800

    webapp basic authentication
    webapp basic authentication

---
+++ springboot/webapp-basicauth/src/main/java/com/b5wang/architect/springboot/security/webapp/basicauth/WebAppBasicAuthApplication.java
@@ -0,0 +1,15 @@
+package com.b5wang.architect.springboot.security.webapp.basicauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+@SpringBootApplication
+public class WebAppBasicAuthApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(WebAppBasicAuthApplication.class,args);
+    }
+
+}
