28873bf27e858d95143e5bbf0c200012fef8e547
B5WANG <neo.b5wang@gmail.com>
2024-03-18 23:17:14 +0800

    Fix sentinel failover issue
    Fix sentinel failover issue

--- redis/redis-linux/redis-sentinel-java/src/main/java/com/b5wang/architect/redis/sentinel/config/RedisConfig.java
+++ redis/redis-linux/redis-sentinel-java/src/main/java/com/b5wang/architect/redis/sentinel/config/RedisConfig.java
@@ -1,30 +1,26 @@
 package com.b5wang.architect.redis.sentinel.config;
 
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.data.redis.RedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisSentinelConfiguration;
 import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 
+/**
+ * https://docs.spring.io/spring-data/redis/reference/redis/connection-modes.html
+ * https://stackoverflow.com/questions/42088184/redis-sentinel-set-up-with-spring-boot
+ *
+ * This is correct:
+ * https://medium.com/javarevisited/caching-with-spring-boot-3-lettuce-and-redis-sentinel-5f6fab7e58f8
+ * */
+
+@Slf4j
 @Configuration
 public class RedisConfig {
-
-    @Bean
-    public RedisConnectionFactory redisConnectionFactory(RedisProperties props) {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
-        config.setPassword(props.getPassword());
-        return new LettuceConnectionFactory(config);
-    }
-
-    @Bean
-    public RedisTemplate<String,Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<String,Object> template = new RedisTemplate<>();
-        template.setConnectionFactory(redisConnectionFactory);
-        return template;
-    }
-
 
 }
