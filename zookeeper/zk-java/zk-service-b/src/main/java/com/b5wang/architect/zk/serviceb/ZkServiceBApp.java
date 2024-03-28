3961a54f38c3fe17369dbc20132eb9487c44edbb
B5WANG <neo.b5wang@gmail.com>
2024-03-28 21:07:46 +0800

    spring boot zookeeper service discovery
    spring boot zookeeper service discovery

--- zookeeper/zk-java/zk-service-b/src/main/java/com/b5wang/architect/zk/serviceb/ZkServiceBApp.java
+++ zookeeper/zk-java/zk-service-b/src/main/java/com/b5wang/architect/zk/serviceb/ZkServiceBApp.java
@@ -1,8 +1,13 @@
 package com.b5wang.architect.zk.serviceb;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class ZkServiceBApp {
+
+    public static void main(String[] args){
+        SpringApplication.run(ZkServiceBApp.class,args);
+    }
+
 }
