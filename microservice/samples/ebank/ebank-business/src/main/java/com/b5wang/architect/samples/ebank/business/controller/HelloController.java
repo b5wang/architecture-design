038f25c681a25d2b0973a6c4b4911c0a52c56489
B5WANG <neo.b5wang@gmail.com>
2024-07-03 10:24:13 +0800

    microservice pass parameter
    microservice pass parameter

--- microservice/samples/ebank/ebank-business/src/main/java/com/b5wang/architect/samples/ebank/business/controller/HelloController.java
+++ microservice/samples/ebank/ebank-business/src/main/java/com/b5wang/architect/samples/ebank/business/controller/HelloController.java
@@ -2,6 +2,7 @@ package com.b5wang.architect.samples.ebank.business.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,9 +11,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${test.arg1}")
+    private String arg1;
+
+    @Value("${test.arg2}")
+    private String arg2;
+
     @GetMapping("/hello")
     public ResponseEntity hello(){
-        log.info("say Hello!");
+        log.info("say Hello! arg1={},arg2={}",arg1,arg2);
         return ResponseEntity.ok("Hello!");
     }
 
