334aede5d030d6c4da9e733e3b713d34d4d1d974
B5WANG <neo.b5wang@gmail.com>
2024-11-02 21:48:33 +0800

    springboot3+redis config
    springboot3+redis config

--- microservice/ratelimiting/src/main/java/com/b5wang/architect/ratelimiting/conf/RedisConfig.java
+++ microservice/ratelimiting/src/main/java/com/b5wang/architect/ratelimiting/conf/RedisConfig.java
@@ -1,37 +1,52 @@
 package com.b5wang.architect.ratelimiting.conf;
 
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
 
-    @Autowired
-    private RedisConnectionFactory redisConnectionFactory;
-
     // 默认用的是用JdkSerializationRedisSerializer进行序列化的
     @Bean
-    public RedisTemplate<String, Object> redisTemplate() {
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         // 注入数据源
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        // 使用Jackson2JsonRedisSerialize 替换默认序列化
-        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
-        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
-        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
-        // key-value结构序列化数据结构
-        redisTemplate.setKeySerializer(stringRedisSerializer);
-        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
-        // hash数据结构序列化方式,必须这样否则存hash 就是基于jdk序列化的
-        redisTemplate.setHashKeySerializer(stringRedisSerializer);
-        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
-        // 启用默认序列化方式
-        redisTemplate.setEnableDefaultSerializer(true);
-        redisTemplate.setDefaultSerializer(jackson2JsonRedisSerializer);
+//        redisTemplate.setConnectionFactory(factory);
+//        // 使用Jackson2JsonRedisSerialize 替换默认序列化
+//        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+//        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+//        objectMapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+//        jackson2JsonRedisSerializer.setObjectMapper(objectMapper);
+//        // key-value结构序列化数据结构
+//        redisTemplate.setKeySerializer(stringRedisSerializer);
+//        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+//        // hash数据结构序列化方式,必须这样否则存hash 就是基于jdk序列化的
+//        redisTemplate.setHashKeySerializer(stringRedisSerializer);
+//        redisTemplate.setHashValueSerializer(jackson2JsonRedisSerializer);
+//        // 启用默认序列化方式
+//        redisTemplate.setEnableDefaultSerializer(true);
+//        redisTemplate.setDefaultSerializer(jackson2JsonRedisSerializer);
+//        redisTemplate.afterPropertiesSet();
 
-        /// redisTemplate.afterPropertiesSet();
+
+        redisTemplate.setConnectionFactory(factory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
+        redisTemplate.afterPropertiesSet();
+
         return redisTemplate;
     }
 
--- microservice/ratelimiting/src/main/java/com/b5wang/architect/ratelimiting/controller/GreetingController.java
+++ microservice/ratelimiting/src/main/java/com/b5wang/architect/ratelimiting/controller/GreetingController.java
@@ -6,8 +6,10 @@ import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.google.common.util.concurrent.RateLimiter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
@@ -16,6 +18,9 @@ public class GreetingController {
 
     @Autowired
     private RateLimiter greetingRateLimiter;
+
+    @Autowired
+    private RedisTemplate<String,String> redisTemplate;
 
     @GetMapping("/hello")
     public ResponseEntity<String> sayHello(){
@@ -48,6 +53,16 @@ public class GreetingController {
         return ResponseEntity.ok("OK");
     }
 
+    @GetMapping("/addKeyAndValue")
+    public ResponseEntity<String> addKeyAndValue(@RequestParam String key,@RequestParam String value){
+        log.info("Set into redis, key: {}, value: {}",key,value);
+        try{
+            redisTemplate.opsForValue().set(key,value);
+        }catch(Exception ex){
+            ex.printStackTrace();
+        }
 
+        return ResponseEntity.ok("OK");
+    }
 
 }
